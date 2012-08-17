9a550adae43ba9f145f16668923689963120c1a4
Eyal Shalev <eyalsh@gmail.com>
2012-08-17 04:20:13 +0300

    Initial Distributions

---
+++ src/eyal/tomcat/quick/Main.java
@@ -0,0 +1,90 @@
+package eyal.tomcat.quick;
+
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+	    System.setProperty("apple.laf.useScreenMenuBar", "true");
+		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Tomcat Quick");
+		if (!SystemTray.isSupported()) {
+			JOptionPane
+					.showMessageDialog(null, "System Tray is not supported.");
+			System.exit(1);
+		}
+
+		SystemTray tray = SystemTray.getSystemTray();
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		URL url = Main.class.getResource("trayIcon.png");
+		Image image = toolkit.getImage(url);
+		toolkit.prepareImage(image, -1, -1, null);
+
+		PopupMenu menu = new PopupMenu();
+
+		MenuItem startTomcat = new MenuItem("Start Tomcat");
+		startTomcat.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				startup();
+			}
+		});
+		menu.add(startTomcat);
+
+		MenuItem stopTomcat = new MenuItem("Stop Tomcat");
+		stopTomcat.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				shutdown();
+			}
+		});
+		menu.add(stopTomcat);
+
+		MenuItem quit = new MenuItem("Quit Tomcat Controller");
+		quit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		menu.add(quit);
+		TrayIcon icon = new TrayIcon(image, "Tomcat Controller", menu);
+		icon.setImageAutoSize(true);
+
+		tray.add(icon);
+		
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				shutdown();
+			}
+		}));
+	}
+
+	private static void startup() {
+		try {
+			Runtime.getRuntime().exec("/usr/local/tomcat/bin/startup.sh", null,
+					null);
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
+	}
+
+	private static void shutdown() {
+		try {
+			Runtime.getRuntime().exec("/usr/local/tomcat/bin/shutdown.sh",
+					null, null);
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
+	}
+
+}
